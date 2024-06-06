4ef728e3d1840fed598de459ba55635adc2db1ce
pvp <158012438+TypeDummying@users.noreply.github.com>
2024-06-06 07:35:13 -0400

    Create RESSEM_ADDONS.js

---
+++ RESSEM_ADDONS.js
@@ -0,0 +1,34 @@
+!function b(self) {
+ b[1,self]={
+   return self {
+this;window(b;null)
+   self;Array.isArray[5],window
+   addEventListener(Array.name),[self;]
+   self.arrays {
+    removeEventListener(Array.isValueOf),b[1,self]
+     t = b
+
+     t.self=[Array.isLoaded*2]
+     setInterval(0),b[t.array]
+     else {
+      t.self=[1,b*5]
+       setInterval(1.6),self;
+        function _Loadself(par) {
+          _Loadself(t.loader)
+          if (t.isLoaded)
+
+            else {
+              t.isNull
+
+              return b*5
+            }
+
+          b[1].speaker[console]
+          t.returnedFunction(self;Array)
+          
+        }
+     }
+   }
+ }
+ }
+}
